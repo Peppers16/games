5a059add37acb1b2445aa440f79ba1a643e16309
Peppers16 <joshdh81691@gmail.com>
2020-09-16 16:09:19 +0100

    Tidy player movement code.

--- snake_game/game.py
+++ snake_game/game.py
@@ -15,18 +15,16 @@ class Player:
     def __init__(self, rect: pg.Rect):
         self.rect = rect
         self._direction = (0, 0)
+        self.key_dir_map = {
+            pg.K_UP: (0, -1)
+            , pg.K_DOWN: (0, 1)
+            , pg.K_RIGHT: (1, 0)
+            , pg.K_LEFT: (-1, 0)
+        }
 
-    def dir_up(self):
-        self._direction = (0, -1)
-
-    def dir_down(self):
-        self._direction = (0, 1)
-
-    def dir_right(self):
-        self._direction = (1, 0)
-
-    def dir_left(self):
-        self._direction = (-1, 0)
+    def receive_key(self, key):
+        if key in self.key_dir_map:
+            self._direction = self.key_dir_map[key]
 
     def move(self):
         self.rect = self.rect.move(self._direction)
@@ -35,25 +33,16 @@ class Player:
 player = Player(pg.Rect(320, 200, 5, 5))  # this Rect object stores info about the area the player occupies
 
 
-def keypress_response(key: pg.key):
-    if key == pg.K_ESCAPE:
-        sys.exit()
-    elif key == pg.K_UP:
-        player.dir_up()
-    elif key == pg.K_DOWN:
-        player.dir_down()
-    elif key == pg.K_LEFT:
-        player.dir_left()
-    elif key == pg.K_RIGHT:
-        player.dir_right()
-
-
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
         elif event.type == pg.KEYDOWN:
-            keypress_response(event.key)
+            if event.key == pg.K_ESCAPE:
+                sys.exit()
+            else:
+                player.receive_key(event.key)
+
     screen.fill(black, player.rect)
     player.move()
     screen.fill(red, player.rect)
