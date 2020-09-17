1b3f6713a145a14e2bf8594598573f5ab896f06c
Peppers16 <joshdh81691@gmail.com>
2020-09-17 11:42:01 +0100

    Created Cell and Grid classes for representing the game board. Cells draw themselves on the screen.

--- snake_game/game.py
+++ snake_game/game.py
@@ -1,11 +1,14 @@
 import pygame as pg
 import sys
 from numpy import array
+from collections import deque
 
 pg.init()
 
-red = (255, 0, 0)
-black = (0, 0, 0)
+snake_color = (255, 0, 0)
+bg_color = (0, 0, 0)
+deadzone_color = (0, 255, 0)
+
 screen_width = 500
 cell_width = 5
 
@@ -13,11 +16,55 @@ screen = pg.display.set_mode((screen_width, screen_width))
 clock = pg.time.Clock()
 
 
+class Cell:
+    def __init__(self, rect: pg.Rect, is_deadzone=False, draw_color=bg_color):
+        self.contains_snake = False
+        self.contains_food = False
+        self.is_deadzone = is_deadzone
+        self.draw_color = draw_color
+        self.rect = rect
+
+    def draw(self):
+        screen.fill(self.draw_color, self.rect)
+
+
+class Grid:
+    def __init__(self):
+        self.grid = []
+        self.grid_width = screen_width // cell_width
+        for r in range(self.grid_width):
+            row = []
+            for c in range(self.grid_width):
+                if r in [0, self.grid_width - 1] or c in [0, self.grid_width - 1]:
+                    deadzone = True
+                    color = deadzone_color
+                else:
+                    deadzone = False
+                    color = bg_color
+                row.append(
+                    Cell(
+                        rect=pg.Rect(c*cell_width, r*cell_width, cell_width, cell_width)
+                        , is_deadzone=deadzone
+                        , draw_color=color
+                    ))
+            self.grid.append(row)
+        self._draw_screen()
+
+    def _draw_screen(self):
+        for cell in self:
+            cell.draw()
+
+    def __getitem__(self, item):
+        return self.grid[item]
+
+    def __iter__(self):
+        return (c for r in self.grid for c in r)
+
+
 class Player:
     def __init__(self, rect: pg.Rect):
         self.rect = rect
         self._direction = array([0, 0])
-        self.speed = 1
         self.key_dir_map = {
             pg.K_UP: array([0, -1])
             , pg.K_DOWN: array([0, 1])
@@ -32,12 +79,12 @@ class Player:
                 self._direction = new_dir
 
     def move(self):
-        velocity = self._direction * self.speed * cell_width
+        velocity = self._direction * cell_width
         self.rect = self.rect.move(tuple(velocity))
 
 
-player = Player(pg.Rect(screen_width/2, screen_width/2, cell_width, cell_width))  # this Rect object stores info about the area the player occupies
-
+player = Player(pg.Rect(screen_width / 2, screen_width / 2, cell_width, cell_width))
+grid = Grid()
 
 while True:
     for event in pg.event.get():
@@ -48,9 +95,8 @@ while True:
                 sys.exit()
             else:
                 player.receive_key(event.key)
-
-    screen.fill(black, player.rect)
+    screen.fill(bg_color, player.rect)
     player.move()
-    screen.fill(red, player.rect)
+    screen.fill(snake_color, player.rect)
     pg.display.update()
     clock.tick(30)
